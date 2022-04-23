1856d75727e7b5a0cfebe84921b655f9d2736808
t-k-satoh <syakuyanaoto@gmail.com>
2022-04-23 22:46:06 +0900

    chore(package): add jest

---
+++ src/index.test.ts
@@ -0,0 +1,9 @@
+import { testFunction } from "./index";
+
+describe(testFunction.name, () => {
+  test("should return value", () => {
+    const value = "value";
+
+    expect(testFunction(value)).toBe(value);
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export const testFunction = (value: string) => value;
