e7698afa404dc9cb5c0bda1450dec75d3a7d9d5d
t-k-satoh <syakuyanaoto@gmail.com>
2022-04-29 21:56:44 +0900

    feat(page): mod getServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
 import { NextPage, GetServerSideProps } from 'next'
 import Head from 'next/head'
+import { Prefecture } from '../src/domain/entity/prefectures'
 import { prefecturesUseCase } from '../src/use-case/prefectures'
+import { createAPIConfig } from '../src/utils/api'
 
-const Home: NextPage = () => {
+interface Props {
+  prefectures: Prefecture[]
+  error?: unknown
+}
+
+const Home: NextPage<Props> = (props) => {
   const { data } = prefecturesUseCase.useGetPrefectures()
 
   return (
@@ -11,13 +18,27 @@ const Home: NextPage = () => {
         <title>フロントエンドコーディング試験</title>
       </Head>
       {JSON.stringify(data)}
+      {JSON.stringify(props)}
     </>
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    props: {},
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const config = createAPIConfig()
+  try {
+    const data = await prefecturesUseCase.getPrefectures(config)
+    return {
+      props: {
+        prefectures: data,
+      },
+    }
+  } catch (error) {
+    return {
+      props: {
+        prefectures: [],
+        error,
+      },
+    }
   }
 }
 
